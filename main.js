4a330dfb5320e1a32f3e4eed7aa5335a871f39be
AMM9394 <15827061459@163.com>
2020-03-05 15:33:21 +0800

    #2

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ const {app, BrowserWindow} = require('electron');
 
 app.on('ready', () => {
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 900,
     webPreferences: {
+      devTools: true,
       nodeIntegration: true,
     }
   });
+  mainWindow.webContents.openDevTools();
   mainWindow.loadFile('index.html');
   /*const secondWindow = new BrowserWindow({
     width: 400,
@@ -16,6 +18,6 @@ app.on('ready', () => {
       nodeIntegration: true,
     },
     parent: mainWindow,
-  });*/
-  // secondWindow.loadFile('second.html');
+  });
+  secondWindow.loadFile('second.html');*/
 });
--- renderer.js
+++ renderer.js
@@ -1,5 +1,8 @@
-alert(process.versions.node);
-
+/*alert(process.versions.node);
 window.addEventListener('DOMContentLoaded', () => {
   alert('hello world');
+});*/
+window.addEventListener('click', () => {
+  alert('you clicked the window');
 });
+window.alert('script');
