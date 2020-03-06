fb10ecdac8ac12739a1d12127c6154d9d980686d
AMM9394 <15827061459@163.com>
2020-03-06 17:06:01 +0800

    simple music tool

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const {app, BrowserWindow, ipcMain, dialog} = require('electron');
+const DataStore = require('./renderer/MusicDataStore');
 
+const myStore = new DataStore({'name': 'Music Data'});
 class AppWindow extends BrowserWindow {
   constructor(config, fileLocation) {
     const baseConfig = {
@@ -13,6 +15,7 @@ class AppWindow extends BrowserWindow {
     const finalConfig = {...baseConfig, ...config};
     super(finalConfig);
     this.loadFile(fileLocation);
+    this.webContents.openDevTools();
     this.once('ready-to-show', () => {
       this.show();
     });
@@ -21,20 +24,32 @@ class AppWindow extends BrowserWindow {
 
 app.on('ready', () => {
   const mainWindow = new AppWindow({}, './renderer/index.html');
-  mainWindow.webContents.openDevTools();
   ipcMain.on('add-music-button', (event, arg) => {
     const addWindow = new AppWindow({
-      width: 400,
-      height: 500,
+      width: 800,
+      height: 600,
       parent: mainWindow,
     }, './renderer/add.html');
   });
-  ipcMain.on('open-music-file', ((event, arg) => {
+
+  ipcMain.on('add-tracks', (event, tracks) => {
+    const updateTracks = myStore.addTracks(tracks).getTracks();
+    console.log(updateTracks);
+  });
+
+  ipcMain.on('open-music-file', (event, arg) => {
     dialog.showOpenDialog({
       properties: ['openFile', 'multiSelections'],
       filters: [{name: 'Music', extensions: ['mp3']}],
-    }, files => {
+    }, (files) => {
       console.log(files);
+
+    }).then(result => {
+      // console.log(result.canceled);
+      if (!result.canceled) {
+        event.sender.send('selected-file', result.filePaths);
+      }
     });
-  }));
+  });
+
 });
---
+++ renderer/MusicDataStore.js
@@ -0,0 +1,37 @@
+const Store = require('electron-store');
+const uuidv4 = require('uuid/v4');
+const path = require('path');
+
+class DataStore extends Store {
+  constructor(settings) {
+    super(settings);
+    this.tracks = this.get('tracks') || [];
+  }
+
+  saveTracks() {
+    this.set('tracks', this.tracks);
+    return this;
+  }
+
+  getTracks() {
+    return this.get('tracks') || [];
+  }
+
+  addTracks(tracks) {
+    const trackWidthProps = tracks.map(track => {
+      return {
+        id: uuidv4(),
+        path: track,
+        fileName: path.basename(track),
+      };
+    }).filter(track => {
+      const currentTracksPath = this.getTracks().map(track => track.path);
+      return !currentTracksPath.includes(track.path);
+    });
+    this.tracks = [...this.tracks, ...trackWidthProps];
+    return this.saveTracks();
+  }
+
+}
+
+module.exports = DataStore;
